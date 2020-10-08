5680837d0d02c98da2c3b7557e86c37dd2285367
Alfredo Martinez <psialfredomtz@gmail.com>
2020-10-07 23:26:57 -0500

    Store

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import {Provider} from 'react-redux'
 import { Pokemons } from './components/Pokemons';
+import generateStore from './redux/store'
 
 
 export function App() {
+  const store = generateStore()
   return (
-    <Provider >
+    <Provider store={store} >
       <Pokemons/>
     </Provider>
   );
