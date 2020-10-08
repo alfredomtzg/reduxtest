4a02379d6266a86e26b6e90cbe442780221fd409
Alfredo Martinez <psialfredomtz@gmail.com>
2020-10-08 00:00:23 -0500

    Bring in list pokemons

--- src/components/Pokemons.jsx
+++ src/components/Pokemons.jsx
@@ -4,10 +4,22 @@ import { getPokemonAction } from "../redux/pokeDuck";
 
 export const Pokemons = () => {
   const dispatch = useDispatch();
+  const pokemons = useSelector(store => store.pokemones.array)
+  console.log(pokemons)
+
   return (
     <>
       <h1>list of pokemons</h1>
       <button onClick={()=> dispatch(getPokemonAction())} >Get Pokemons</button>
+      <ul>
+        {
+          pokemons.map(item => (
+            <li key={item.name}>
+              {item.name}
+            </li>
+          ))
+        }
+      </ul>
     </>
     )
 };
